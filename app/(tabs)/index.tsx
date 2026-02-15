867237ae98c9c0ff8d36059a24806e1246f85fc6
Robert Chijioke <robertchijiokeogo@gmail.com>
2026-02-15 07:10:59 +0000

    Initial FuturistekApp version

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 
 export default function HomeScreen() {
